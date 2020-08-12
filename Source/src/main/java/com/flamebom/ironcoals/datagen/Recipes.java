2666e648b11477a1e65ccb0416db12aaaa01ae50
Flamebom <kylechess8@gmail.com>
2020-08-12 16:04:05 -0700

    Added Recipes and langs

--- Source/src/main/java/com/flamebom/ironcoals/datagen/Recipes.java
+++ Source/src/main/java/com/flamebom/ironcoals/datagen/Recipes.java
@@ -10,57 +10,61 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.tags.ItemTags;
 import net.minecraftforge.common.Tags;
-
+import net.minecraft.item.Items;
 public class Recipes extends RecipeProvider {
 	public Recipes(DataGenerator generatorIn) {
 		super(generatorIn);
 	}
 	 @Override
 	    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
-	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.IRONCOAL.get())
+	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.IRONCOAL.get(),8)
 	         .patternLine("aaa")
 	         .patternLine("a#a")
 	         .patternLine("aaa")
 	         .key('a', ItemTags.COALS)
-	         .key('#', Tags.Items.STORAGE_BLOCKS_COAL)
+	         .key('#', Tags.Items.STORAGE_BLOCKS_IRON)
+	         .setGroup("IronCoals")
+	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
+	      
+	         .build(consumer);
+	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.GOLDCOAL.get(),8)
+	         .patternLine("aaa")
+	         .patternLine("a#a")
+	         .patternLine("aaa")
+	         .key('a', ItemRegistration.IRONCOAL.get())
+	         .key('#', Tags.Items.STORAGE_BLOCKS_GOLD)
 	         .setGroup("IronCoals")
 	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
 	         .build(consumer);
-	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.GOLDCOAL.get())
+	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.DIAMONDCOAL.get(),8)
 	         .patternLine("aaa")
 	         .patternLine("a#a")
 	         .patternLine("aaa")
-	         .key('a', ItemTags.COALS)
-	         .key('#', Tags.Items.STORAGE_BLOCKS_COAL)
+	         .key('a', ItemRegistration.GOLDCOAL.get())
+	         .key('#', Tags.Items.STORAGE_BLOCKS_DIAMOND)
 	         .setGroup("IronCoals")
 	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
 	         .build(consumer);
-	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.DIAMONDCOAL.get())
+	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.EMERALDCOAL.get(),8)
 	         .patternLine("aaa")
 	         .patternLine("a#a")
 	         .patternLine("aaa")
-	         .key('a', ItemTags.COALS)
-	         .key('#', Tags.Items.STORAGE_BLOCKS_COAL)
-	         .setGroup("IronCoals")
-	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
-	         .build(consumer);
-	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.EMERALDCOAL.get())
-	         .patternLine("aaa")
-	         .patternLine("a#a")
-	         .patternLine("aaa")
-	         .key('a', ItemTags.COALS)
-	         .key('#', Tags.Items.STORAGE_BLOCKS_COAL)
+	         .key('a', ItemRegistration.DIAMONDCOAL.get())
+	         .key('#', Tags.Items.STORAGE_BLOCKS_EMERALD)
 	         .setGroup("IronCoals")
 	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
 	         .build(consumer);
 	        ShapedRecipeBuilder.shapedRecipe(ItemRegistration.AEONCOAL.get())
-	         .patternLine("aaa")
-	         .patternLine("a#a")
-	         .patternLine("aaa")
-	         .key('a', ItemTags.COALS)
-	         .key('#', Tags.Items.STORAGE_BLOCKS_COAL)
+	         .patternLine("aba")
+	         .patternLine("x#x")
+	         .patternLine("aba")
+	         .key('a', ItemRegistration.EMERALDCOAL.get())
+	         .key('#', Items.NETHERITE_BRICKS)
+	         .key('x', Items.NETHER_STAR)
+	         .key('b', Items.HEART_OF_THE_SEA)
 	         .setGroup("IronCoals")
 	         .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
 	         .build(consumer);
