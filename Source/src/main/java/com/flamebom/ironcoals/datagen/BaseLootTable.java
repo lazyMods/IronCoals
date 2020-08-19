6cada3a5e76ba99ad667d238c8b5cb44f8b6c96e
Kyle <kylechess8@gmail.com>
2020-08-18 20:40:15 -0700

    added block forms for most things

--- Source/src/main/java/com/flamebom/ironcoals/datagen/BaseLootTable.java
+++ Source/src/main/java/com/flamebom/ironcoals/datagen/BaseLootTable.java
@@ -41,9 +41,6 @@ public abstract class BaseLootTable extends LootTableProvider {
 	                .name(name)
 	                .rolls(ConstantRange.of(1))
 	                .addEntry(ItemLootEntry.builder(block)
-	                        .acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY))
-	                        .acceptFunction(SetContents.builderIn()
-	                                .addLootEntry(DynamicLootEntry.func_216162_a(new ResourceLocation("minecraft", "contents"))))
 	                );
 	        return LootTable.builder().addLootPool(builder);
 	    }
