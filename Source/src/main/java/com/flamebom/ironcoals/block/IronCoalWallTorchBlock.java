79afc7998b63bddff04974df21a829a5f623ed78
Flamebom <kylechess8@gmail.com>
2021-03-15 08:55:18 -0700

    Added torch particle to torch

--- Source/src/main/java/com/flamebom/ironcoals/block/IronCoalWallTorchBlock.java
+++ Source/src/main/java/com/flamebom/ironcoals/block/IronCoalWallTorchBlock.java
@@ -1,18 +1,18 @@
 package com.flamebom.ironcoals.block;
 
 import com.flamebom.ironcoals.setup.BlockRegistration;
+import com.flamebom.ironcoals.setup.ParticleRegistration;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.WallTorchBlock;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
 public class IronCoalWallTorchBlock extends WallTorchBlock {
 	public IronCoalWallTorchBlock() {
-		super(Block.Properties.from(Blocks.WALL_TORCH).lootFrom(BlockRegistration.IRONCOALTORCH.get()), ParticleTypes.END_ROD);
+		super(Block.Properties.from(Blocks.WALL_TORCH).lootFrom(BlockRegistration.IRONCOALTORCH.get()), ParticleRegistration.TORCHPARTICLE.get());
 	}
 
 	@Override
