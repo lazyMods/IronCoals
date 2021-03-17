2b07696b99670af33a9fba8cb6a259bd332f7d21
Flamebom <kylechess8@gmail.com>
2021-03-17 09:13:17 -0700

    Making Particle Changes

--- Source/src/main/java/com/flamebom/ironcoals/factories/TorchParticleFactory.java
+++ Source/src/main/java/com/flamebom/ironcoals/factories/TorchParticleFactory.java
@@ -1,25 +1,31 @@
 package com.flamebom.ironcoals.factories;
 
+import javax.annotation.Nullable;
+
 import com.flamebom.ironcoals.particles.TorchParticle;
 
 import net.minecraft.client.particle.IParticleFactory;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particles.BasicParticleType;
+import net.minecraft.util.text.Color;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class TorchParticleFactory implements IParticleFactory<BasicParticleType> {
-
-	public TorchParticleFactory() {
-
-	}
-
+public  class TorchParticleFactory implements IParticleFactory<BasicParticleType> {
+	  private final Color color;
+	  public TorchParticleFactory(Color color) {
+	    this.color = color;
+	  }
+	@Override
 	public Particle makeParticle(BasicParticleType typeIn, ClientWorld worldIn, double x, double y, double z,
 			double xSpeed, double ySpeed, double zSpeed) {
-		TorchParticle particle = new TorchParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed);
-		particle.setColor(.1F, .5f, .5F);
-		return particle;
+		// TODO Auto-generated method stub
+		return null;
 	}
-}
+
+
+
+	}
